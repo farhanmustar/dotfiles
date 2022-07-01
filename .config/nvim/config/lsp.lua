c762f290a3632f12805a25eaf13741dff9852006
Farhan Mustar <farhanmustar@gmail.com>
2022-07-01 08:27:52 +0800

    Refactor lsp.lua config file and add roslint pep8 config.

--- .config/nvim/config/lsp.lua
+++ .config/nvim/config/lsp.lua
@@ -5,22 +5,46 @@ if not ok then
 end
 
 local null_ls = require('null-ls')
-null_ls.setup({
-  sources = {
-    -- Python
-    null_ls.builtins.diagnostics.flake8.with({
-      -- more strict mode
-      -- extra_args = {'--max-line-length=199', '--ignore=W504,E128'},
-      extra_args = {'--max-line-length=199', '--ignore=W606,W605,W504,E128,F841,E731'},
-    }),
-    null_ls.builtins.formatting.autopep8.with({
-      extra_args = {'-aa', '--max-line-length=199', '--ignore=E128,E722'},
-    }),
+local helpers = require('null-ls.helpers')
+null_ls.setup({})
 
-    -- Html
-    -- null_ls.builtins.formatting.djlint, -- python 3
-  },
+-- Python
+local flake8 = null_ls.builtins.diagnostics.flake8.with({
+  -- extra_args = {'--max-line-length=199', '--ignore=W504,E128'}, -- more strict mode
+  extra_args = {'--max-line-length=199', '--ignore=W606,W605,W504,E128,F841,E731'},
 })
+local autopep8 = null_ls.builtins.formatting.autopep8.with({
+  extra_args = {'-aa', '--max-line-length=199', '--ignore=E128,E722'},
+})
+local roslint_pep8 = {
+  name = 'roslint_pep8',
+  method = null_ls.methods.DIAGNOSTICS,
+  filetypes = {'python',},
+  generator = null_ls.generator({
+    command = 'rosrun',
+    args = {'roslint', 'pep8', '-'},
+    to_stdin = true,
+    format = 'line',
+		check_exit_code = function(code, stderr)
+			print(stderr)
+			return code <= 1
+		end,
+    on_output = helpers.diagnostics.from_patterns({
+      {
+        pattern = [[[^:]:(%d+):(%d+): [%w-/]+ (.*)]],
+        groups = { "row", "col", "message" },
+      },
+    }),
+  }),
+}
+null_ls.register(flake8)
+null_ls.register(autopep8)
+if vim.fn.executable('rosrun') ~= 0 then
+  null_ls.register(roslint_pep8)
+end
+
+-- Html
+-- local djlint = null_ls.builtins.formatting.djlint, -- python 3
 
 -- Keyboard Shortcut
 vim.keymap.set('n', '<leader>af', vim.lsp.buf.format)
