0dfab93e796d7dbc6db00835a78ba8e9c25b42b3
Farhan Mustar <farhanmustar@gmail.com>
2022-09-03 15:18:53 +0800

    Update wezterm config.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,8 @@ return {
   text_background_opacity = 0.7,
   tab_bar_at_bottom = true,
   use_fancy_tab_bar = false,
-  font = wezterm.font('Comic Code Ligatures', { weight = 'Medium' }),
+  font = wezterm.font('Comic Code Ligatures Medium'),
+  cell_width = 0.85,
   font_size = 9.0,
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' },
@@ -25,5 +26,13 @@ return {
     { key = 'F2', mods = '', action = wezterm.action.SpawnTab('CurrentPaneDomain') },
     { key = 'F3', mods = '', action = wezterm.action.ActivateTabRelative(-1) },
     { key = 'F4', mods = '', action = wezterm.action.ActivateTabRelative(1) },
-  }
+    { key = 'p', mods = 'LEADER', action = wezterm.action.PasteFrom('PrimarySelection') },
+  },
+  mouse_bindings = {
+    {
+      event = { Up = { streak = 1, button = 'Right' } },
+      mods = 'SHIFT',
+      action = wezterm.action.PasteFrom('Clipboard'),
+    },
+  },
 }
