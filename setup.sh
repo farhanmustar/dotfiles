2a5f1f4777f20a965291c8e83c092608d884f830
Farhan Mustar <farhanmustar@gmail.com>
2022-11-18 10:03:12 +0800

    Add setup script.

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+mkdir ~/.config
+ln -sT $SCRIPTPATH/.bash_aliases ~/.bash_aliases
+ln -sT $SCRIPTPATH/.config/nvim/ ~/.config/nvim
+ln -sT $SCRIPTPATH/.gitconfig ~/.gitconfig
+ln -sT $SCRIPTPATH/.byobu/ ~/.byobu
+ln -sT $SCRIPTPATH/.gitignore_global ~/.gitignore_global
+
+read -p "Install neovim? (y/n) : " yn
+if [ "$yn" = "y" ]; then
+  sudo add-apt-repository ppa:neovim-ppa/unstable -yu
+  sudo apt-get install neovim -y
+  sudo update-alternatives --install $(which vim) vim $(which nvim) 50
+  sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 100
+fi
