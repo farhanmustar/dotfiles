66b3d720643f5fd7f5fd4513c601a56e12c925bc
Farhan Mustar <farhanmustar@gmail.com>
2022-12-24 04:50:47 +0800

    Add rest nvim plugin.

--- .config/nvim/config/plugin.lua
+++ .config/nvim/config/plugin.lua
@@ -119,3 +119,52 @@ require("nvim-tree").setup({
   },
 })
 vim.keymap.set('n', '<leader>ft', function() require("nvim-tree.api").tree.toggle(true, true, vim.fn.expand('%:p:h')) end)
+
+-- rest.nvim config
+require("rest-nvim").setup({
+  -- Open request results in a horizontal split
+  result_split_horizontal = false,
+  -- Keep the http file buffer above|left when split horizontal|vertical
+  result_split_in_place = false,
+  -- Skip SSL verification, useful for unknown certificates
+  skip_ssl_verification = false,
+  -- Encode URL before making request
+  encode_url = true,
+  -- Highlight request on run
+  highlight = {
+    enabled = true,
+    timeout = 150,
+  },
+  result = {
+    -- toggle showing URL, HTTP info, headers at top the of result window
+    show_url = true,
+    show_http_info = true,
+    show_headers = true,
+    -- executables or functions for formatting response body [optional]
+    -- set them to nil if you want to disable them
+    formatters = {
+      json = function(body)
+        return vim.fn.system({"jq", "."}, body)
+      end,
+      html = function(body)
+        return vim.fn.system({"tidy", "-i", "-q", "-"}, body)
+      end
+    },
+  },
+  -- Jump to request line on run
+  jump_to_request = false,
+  env_file = '.env',
+  custom_dynamic_variables = {},
+  yank_dry_run = true,
+})
+local rest_map_group = vim.api.nvim_create_augroup("restMap", { clear = true })
+function rest_map()
+  vim.keymap.set('n', '<leader>bu', '<Plug>RestNvim', {buffer = true})
+  vim.keymap.set('n', '<leader>bp', '<Plug>RestNvimPreview', {buffer = true})
+  vim.keymap.set('n', '<leader>bb', '<Plug>RestNvimLast', {buffer = true})
+end
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "http",
+  callback = rest_map,
+  group = rest_map_group,
+})
