548352955e8200175f113ae9545dc18e9baa9db2
Farhan Mustar <farhanmustar@gmail.com>
2024-08-14 18:21:40 +0800

    Add shellcheck linter for posix script to end my misery.

--- .config/nvim/config/lsp.lua
+++ .config/nvim/config/lsp.lua
@@ -348,3 +348,7 @@ null_ls.register(astyle)
 
 -- c_sharp
 require('lspconfig').csharp_ls.setup({})
+
+-- shell
+null_ls.register(null_ls.builtins.diagnostics.shellcheck)
+null_ls.register(null_ls.builtins.code_actions.shellcheck)
