783a42ce0a37de49b6e2271b90391c7e712fadef
Farhan Mustar <farhanmustar@gmail.com>
2025-01-19 08:49:08 +0800

    Update wezterm config.

--- .config/wezterm_with_byobu/wezterm.lua
+++ .config/wezterm_with_byobu/wezterm.lua
@@ -3,21 +3,53 @@ local wezterm = require('wezterm')
 return {
   default_prog = { '/usr/bin/bash' },
   -- default_prog = { 'wsl.exe', '--cd', '/home/devo' },
-  color_scheme = 'GruvboxDarkHard',
   window_background_opacity = 0.95,
   text_background_opacity = 0.7,
   tab_bar_at_bottom = true,
   hide_tab_bar_if_only_one_tab = true,
   use_fancy_tab_bar = false,
-  font = wezterm.font('Comic Code Ligatures Medium'),
-  cell_width = 0.85,
-  line_height = 1.4,
-  font_size = 10.0,
+
+  window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+  window_padding = {
+    left = 10,
+    right = 1,
+    top = 10,
+    bottom = 0,
+  },
+
+  font = wezterm.font('SeriousShanns Nerd Font Mono'),
+  font_rules = {
+    {
+      intensity = 'Bold',
+      italic = false,
+      font = wezterm.font('SeriousShanns Nerd Font Mono', { weight = 'Bold', stretch = 'Normal', style = 'Normal' }),
+    },
+    {
+      intensity = 'Bold',
+      italic = true,
+      font = wezterm.font('SeriousShanns Nerd Font Mono', { weight = 'Bold', stretch = 'Normal', style = 'Italic' }),
+    },
+    {
+      intensity = 'Normal',
+      italic = true,
+         font = wezterm.font(
+           'SeriousShanns Nerd Font Mono',
+           { weight = 'Regular', stretch = 'Normal', style = 'Italic' }
+         ),
+    },
+  },
+  line_height = 1.05,
+  font_size = 12.0,
+  use_cap_height_to_scale_fallback_fonts = true,
+  underline_position = -2,
+  -- force_reverse_video_cursor = true,
+
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   use_ime = false,
   leader = { key = 'Q', mods = 'CTRL', timeout_milliseconds = 1000 },
   keys = {
     { key = 'Escape', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
+    { key = 'f', mods = 'LEADER', action = wezterm.action.QuickSelect },
     { key = 'v', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal({}) },
     { key = 's', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical({}) },
     { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection('Down') },
@@ -27,6 +59,7 @@ return {
     { key = 'F2', mods = 'LEADER', action = wezterm.action.SpawnTab('CurrentPaneDomain') },
     { key = 'F3', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(-1) },
     { key = 'F4', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(1) },
+    { key = '.', mods = 'LEADER', action = wezterm.action.CharSelect({copy_on_select=false}) },
     { key = 'p', mods = 'LEADER', action = wezterm.action.PasteFrom('Clipboard') },
     { key = 'm', mods = 'LEADER', action = wezterm.action.ToggleFullScreen },
     { key = 'Enter', mods = 'ALT', action = wezterm.action.ToggleFullScreen },
