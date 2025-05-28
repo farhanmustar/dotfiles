313de99ad198ca842d2f4338be9c436b35743435
Farhan Mustar <farhanmustar@gmail.com>
2025-05-28 09:51:14 +0800

    Inject shader highlighting in javascript.

--- .config/nvim/queries/javascript/injections.scm
+++ .config/nvim/queries/javascript/injections.scm
@@ -2,3 +2,12 @@
   name: (identifier) @_name (#match? @_name ".+(Html|Svg|Tpl)$")
   value: (_) @html
 )
+
+(object
+  (pair
+    (property_identifier) @_name (#match? @_name ".+(Shader)$")
+    (template_string
+      (string_fragment) @glsl
+    )
+  )
+)
