56f4d5fe25bf64db797ea58ac894891aec5c3a1d
Farhan Mustar <farhanmustar@gmail.com>
2026-05-28 08:51:57 +0800

    Rearrange wezterm config to make font swap easy.

--- .config/wezterm_with_byobu/wezterm.lua
+++ .config/wezterm_with_byobu/wezterm.lua
@@ -1,47 +1,42 @@
 local wezterm = require('wezterm')
 
-return {
-  default_prog = { '/usr/bin/bash' },
-  -- default_prog = { 'wsl.exe', '--cd', '/home/devo' },
-  window_background_opacity = 0.95,
-  window_decorations = "INTEGRATED_BUTTONS|RESIZE",
-  window_padding = {
-    left = 10,
-    right = 1,
-    top = 10,
-    bottom = 0,
+local FONT = 'serious_shanns'
+-- local FONT = 'pixel_code'
+
+-- local FONT = 'comic_code'
+
+
+local fonts = {}
+
+fonts.pixel_code = {
+  font = wezterm.font('Pixel Code Regular'),
+  font_rules = {
+    {
+      intensity = 'Bold',
+      italic = false,
+      font = wezterm.font('Pixel Code ExtraBold', { weight = 'Bold', stretch = 'Normal', style = 'Normal' }),
+    },
+    {
+      intensity = 'Bold',
+      italic = true,
+      font = wezterm.font('Pixel Code ExtraBold', { weight = 'Bold', stretch = 'Normal', style = 'Italic' }),
+    },
+    {
+      intensity = 'Normal',
+      italic = true,
+         font = wezterm.font(
+           'Pixel Code Thin',
+           { weight = 'Regular', stretch = 'Normal', style = 'Normal' }
+         ),
+    },
   },
-  -- text_background_opacity = 0.7,
-  tab_bar_at_bottom = true,
-  hide_tab_bar_if_only_one_tab = true,
-  use_fancy_tab_bar = false,
+  line_height = 1,
+  font_size = 7.0,
+  underline_position = -2,
+  cell_width = 1.1,
+}
 
-  -- font = wezterm.font('Pixel Code Regular'),
-  -- font_rules = {
-  --   {
-  --     intensity = 'Bold',
-  --     italic = false,
-  --     font = wezterm.font('Pixel Code ExtraBold', { weight = 'Bold', stretch = 'Normal', style = 'Normal' }),
-  --   },
-  --   {
-  --     intensity = 'Bold',
-  --     italic = true,
-  --     font = wezterm.font('Pixel Code ExtraBold', { weight = 'Bold', stretch = 'Normal', style = 'Italic' }),
-  --   },
-  --   {
-  --     intensity = 'Normal',
-  --     italic = true,
-  --        font = wezterm.font(
-  --          'Pixel Code Thin',
-  --          { weight = 'Regular', stretch = 'Normal', style = 'Normal' }
-  --        ),
-  --   },
-  -- },
-  -- line_height = 1,
-  -- font_size = 7.0,
-  -- underline_position = -2,
-  -- cell_width = 1.1,
-
+fonts.serious_shanns = {
   font = wezterm.font('SeriousShanns Nerd Font Mono'),
   font_rules = {
     {
@@ -68,11 +63,30 @@ return {
   use_cap_height_to_scale_fallback_fonts = true,
   underline_position = -2,
   -- force_reverse_video_cursor = true,
+}
 
-  -- font = wezterm.font('Comic Code Ligatures Medium'),
-  -- cell_width = 0.85,
-  -- line_height = 1.4,
-  -- font_size = 10.0,
+fonts.comic_code = {
+  font = wezterm.font('Comic Code Ligatures Medium'),
+  cell_width = 0.85,
+  line_height = 1.4,
+  font_size = 10.0,
+}
+
+local config = {
+  default_prog = { '/usr/bin/bash' },
+  -- default_prog = { 'wsl.exe', '--cd', '/home/devo' },
+  window_background_opacity = 0.95,
+  window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+  window_padding = {
+    left = 10,
+    right = 1,
+    top = 10,
+    bottom = 0,
+  },
+  -- text_background_opacity = 0.7,
+  tab_bar_at_bottom = true,
+  hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = false,
 
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   use_ime = false,
@@ -102,3 +116,7 @@ return {
     },
   },
 }
+
+for k, v in pairs(fonts[FONT]) do config[k] = v end
+
+return config
